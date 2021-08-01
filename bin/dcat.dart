09e3d78e686504e6a77c2120f59d9b5ee2d38db5
Zoheb Alli Khan <understandingzak@gmail.com>
2021-08-01 13:57:48 +0530

    Dcat error working

--- bin/dcat.dart
+++ bin/dcat.dart
@@ -1,3 +1,49 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+
+const lineNumber = 'line-number';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  exitCode = 0;
+  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+
+  // ignore: omit_local_variable_types
+  ArgResults argResults = parser.parse(arguments);
+
+  final paths = argResults.rest;
+
+  dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
+}
+
+Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
+  if (paths.isEmpty) {
+    await stdin.pipe(stdout);
+  } else {
+    for (final path in paths) {
+      var LineNumber = 1;
+      final lines = utf8.decoder
+          .bind(File(path).openRead())
+          .transform(const LineSplitter());
+      try {
+        await for (final line in lines) {
+          if (showLineNumbers) {
+            stdout.write('${LineNumber++} ');
+          }
+          stdout.writeln(line);
+        }
+      } catch (e) {
+        await _handleError(path);
+      }
+    }
+  }
+}
+
+Future<void> _handleError(String path) async {
+  if (await FileSystemEntity.isDirectory(path)) {
+    stderr.writeln('Error: $path is a directory');
+  } else {
+    exitCode = 2;
+  }
 }
